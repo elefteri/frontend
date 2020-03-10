3a8500d1af777bbffa5df49b9a636b1d8945520c
ioannis@miakalimera.gr <ioannis@miakalimera.gr>
2020-03-10 22:16:57 +0200

    one more change for travis

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Yiannhs again</p>
+        <p>Greetings through travis</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
